1f67a9fe553737b8150a36b5c0d12df1cb1fa638
hamzalamin <hamzalamin80@gmail.com>
2024-09-30 02:01:43 +0200

    create the entities of project using anotations

---
+++ src/main/java/com/wora/Entities/Course.java
@@ -0,0 +1,38 @@
+package com.wora.Entitys;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    public Course() {
+    }
+
+    public Course(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ src/main/java/com/wora/Entities/StudentsCourses.java
@@ -0,0 +1,53 @@
+package com.wora.Entitys;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students_courses")
+public class StudentsCourses {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+
+    public StudentsCourses() {
+    }
+
+    public StudentsCourses(Long id, Student student, Course course) {
+        this.id = id;
+        this.student = student;
+        this.course = course;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+}
